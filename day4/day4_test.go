4e8e35a86bc589c11f78cfe7eeb57eea81deac80
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-12-19 23:05:24 +0530

    day 4 part 2

--- day4/day4_test.go
+++ day4/day4_test.go
@@ -27,4 +27,26 @@ MXMXAXMASX`
 			t.Errorf("incorrect answer, got: %v, want: %v", got, want)
 		}
 	})
+
+	t.Run("returns correct answer for part 2", func(t *testing.T) {
+		testString := `.M.S......
+..A..MSMS.
+.M.S.MAA..
+..A.ASMSM.
+.M.S.M....
+..........
+S.S.S.S.S.
+.A.A.A.A..
+M.M.M.M.M.
+..........`
+
+		testInput := strings.Split(testString, "\n")
+
+		want := 9
+		got := GetPart2(testInput)
+
+		if got != want {
+			t.Errorf("incorrect answer, got: %v, want: %v", got, want)
+		}
+	})
 }
--- day4/main.go
+++ day4/main.go
@@ -17,6 +17,7 @@ func main() {
 	}
 
 	fmt.Printf("part 1 answer: %v\n", GetPart1(lines))
+	fmt.Printf("part 2 answer: %v\n", GetPart2(lines))
 }
 
 var directions = [][]int{
@@ -30,12 +31,59 @@ var directions = [][]int{
 	{1, -1},
 }
 
-func GetPart1(lines []string) (xmasCount int) {
-	stringMatrix := make([][]string, 0, len(lines))
+func GetPart2(lines []string) (xmasCount int) {
+	stringMatrix := getAsMatrix(lines)
 
-	for _, line := range lines {
-		stringMatrix = append(stringMatrix, strings.Split(line, ""))
+	xMax := len(stringMatrix) - 1
+	yMax := len(stringMatrix[0]) - 1
+
+	for x := 0; x <= xMax; x++ {
+		for y := 0; y <= yMax; y++ {
+			char := stringMatrix[x][y]
+
+			if char == "A" {
+				howMany := searchForXMASFixed(stringMatrix, x, y, xMax, yMax)
+				xmasCount += howMany
+			}
+		}
 	}
+
+	return xmasCount
+}
+
+func searchForXMASFixed(matrix [][]string, x, y, xMax, yMax int) int {
+	BRx, BRy := x+1, y+1
+	TLx, TLy := x-1, y-1
+
+	BLx, BLy := x-1, y+1
+	TRx, TRy := x+1, y-1
+
+	foundLeft, foundRight := false, false
+
+	if !(isOutOfBounds(BRx, BRy, xMax, yMax) || isOutOfBounds(TLx, TLy, xMax, yMax)) {
+		// 1: MAS left
+		foundLeft = foundLeft || (matrix[BRx][BRy] == "M" && matrix[TLx][TLy] == "S")
+		// 2: SAM left
+		foundLeft = foundLeft || (matrix[BRx][BRy] == "S" && matrix[TLx][TLy] == "M")
+	}
+
+	if !(isOutOfBounds(BLx, BLy, xMax, yMax) || isOutOfBounds(TRx, TRy, xMax, yMax)) {
+		// 3: MAS right
+		foundRight = foundRight || (matrix[BLx][BLy] == "M" && matrix[TRx][TRy] == "S")
+
+		// 4: SAM right
+		foundRight = foundRight || (matrix[BLx][BLy] == "S" && matrix[TRx][TRy] == "M")
+	}
+
+	if foundLeft && foundRight {
+		return 1
+	}
+
+	return 0
+}
+
+func GetPart1(lines []string) (xmasCount int) {
+	stringMatrix := getAsMatrix(lines)
 
 	xMax := len(stringMatrix) - 1
 	yMax := len(stringMatrix[0]) - 1
@@ -52,6 +100,16 @@ func GetPart1(lines []string) (xmasCount int) {
 	}
 
 	return xmasCount
+}
+
+func getAsMatrix(lines []string) [][]string {
+	stringMatrix := make([][]string, 0, len(lines))
+
+	for _, line := range lines {
+		stringMatrix = append(stringMatrix, strings.Split(line, ""))
+	}
+
+	return stringMatrix
 }
 
 func isOutOfBounds(x, y, xMax, yMax int) bool {
