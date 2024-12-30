920bc8f873cf0b3bb7cd37bfeab4adf55c6ec7a3
Hussain Nagaria <hussainbhaitech@gmail.com>
2024-12-30 07:26:05 +0530

    day 5 part 2

--- day5/main.go
+++ day5/main.go
@@ -12,6 +12,7 @@ func main() {
 	updates := utils.GetLines("updates.txt")
 
 	fmt.Printf("part 1: %v\n", GetPart1(rules, updates))
+	fmt.Printf("part 2: %v\n", GetPart2(rules, updates))
 }
 
 func GetPart1(orderingRules, updates []string) (midSum int64) {
@@ -56,7 +57,47 @@ func isUpdateCorrect(printOrder []int64, rules map[int64]utils.Set) (isCorrect b
 }
 
 func GetPart2(orderingRules, updates []string) (midSum int64) {
+	rules := getRules(orderingRules)
+
+	for _, update := range updates {
+		printOrder := utils.Map(strings.Split(update, ","), utils.ToInt)
+		isCorrect := isUpdateCorrect(printOrder, rules)
+
+		if !isCorrect {
+			// correct it
+			printOrder = getCorrectedOrder(printOrder, rules)
+
+			// then sum the mid
+			// find mid and sum it out
+			mid := len(printOrder) / 2
+			midSum += printOrder[mid]
+		}
+	}
+
 	return
+}
+
+func getCorrectedOrder(incorrectOrder []int64, rules map[int64]utils.Set) []int64 {
+	swapped := true
+
+	for swapped {
+		swapped = false
+		for i := 0; i < len(incorrectOrder)-1; i++ {
+			curNum := incorrectOrder[i]
+			next := incorrectOrder[i+1]
+
+			// curNum comes before nextNum
+			// in other words no rule for nextNum before curNum should exist AND
+			afterSet := rules[next]
+			if afterSet.Has(curNum) {
+				// to be swapped!
+				incorrectOrder[i], incorrectOrder[i+1] = incorrectOrder[i+1], incorrectOrder[i]
+				swapped = true
+			}
+		}
+	}
+
+	return incorrectOrder
 }
 
 func getRules(orderingRules []string) (rules map[int64]utils.Set) {
